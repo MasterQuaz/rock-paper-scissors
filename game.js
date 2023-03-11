17f0d8a6d9e7f1d5c6f52236ca701a187aad9848
MasterQuaz <asm63152@yahoo.com>
2023-03-11 10:24:57 -0500

    Make conditions to determine winner and alert outcome

--- game.js
+++ game.js
@@ -18,4 +18,33 @@ getComputerChoice();
 let input = prompt("Make your choice, if you dare...", "Rock, Paper, Scissors...shoot!");
 let playerSelection = input.toLowerCase();
 
-console.log(playerSelection)
+
+if (computerSelection == "Rock" && playerSelection == "rock" || computerSelection == "Paper" && playerSelection == "paper" || computerSelection == "Scissors" && playerSelection == "scissors") {
+    outcome = `We both chose ${computerSelection}, it is a tie!`
+}
+else if (computerSelection == "Rock" && playerSelection == "scissors") {
+    outcome = "You lose! Rock breaks Scissors."
+}
+else if (computerSelection == "Paper" && playerSelection == "rock") {
+    outcome = "You lose! Paper covers Rock."
+}
+else if (computerSelection == "Scissors" && playerSelection == "paper") {
+    outcome = "You lose! Scissors cuts Paper."
+}
+else if (computerSelection == "Rock" && playerSelection == "paper") {
+    outcome = "You win! Paper covers Rock"
+}
+else if (computerSelection == "Paper" && playerSelection == "scissors") {
+    outcome = "You win! Scissors cuts Paper."
+}
+else if (computerSelection == "Scissors" && playerSelection == "rock") {
+    outcome = "You win! Rock breaks Scissors."
+}
+else {
+    outcome = "Trying to cheat huh? I caught you, you lose!"
+}
+
+console.log(computerSelection);
+console.log(playerSelection);
+
+alert(outcome)
